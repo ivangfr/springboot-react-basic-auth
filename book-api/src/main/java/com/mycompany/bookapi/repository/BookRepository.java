767093452725156a99c2548aaa1e7f95b9d60f46
Ivan Franchin <ivan.franchin@takeaway.com>
2022-05-30 11:31:17 +0200

    remove Param annotation workaround BookRepository class

--- book-api/src/main/java/com/mycompany/bookapi/repository/BookRepository.java
+++ book-api/src/main/java/com/mycompany/bookapi/repository/BookRepository.java
@@ -2,7 +2,6 @@ package com.mycompany.bookapi.repository;
 
 import com.mycompany.bookapi.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +11,5 @@ public interface BookRepository extends JpaRepository<Book, String> {
 
     List<Book> findAllByOrderByTitle();
 
-    // Add @Param as a workaround for https://github.com/spring-projects/spring-data-jpa/issues/2512
-    // Remove them when this issue https://github.com/spring-projects/spring-data-jpa/issues/2519 is closed
-    List<Book> findByIsbnContainingOrTitleContainingOrderByTitle(@Param("isbn") String isbn, @Param("title") String title);
+    List<Book> findByIsbnContainingOrTitleContainingOrderByTitle(String isbn, String title);
 }
