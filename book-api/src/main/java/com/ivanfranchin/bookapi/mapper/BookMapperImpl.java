cb2363edc2eb8e48ad53960ef52653ab3d0c928b
Ivan Franchin <ivan.franchin@takeaway.com>
2023-03-16 22:34:47 +0100

    project update - upgrade to springdoc-openapi 2.0.4; - convert project-digram from png image to jpeg; - in mapperImpl, check if the parameter is null; - update README.

--- book-api/src/main/java/com/ivanfranchin/bookapi/mapper/BookMapperImpl.java
+++ book-api/src/main/java/com/ivanfranchin/bookapi/mapper/BookMapperImpl.java
@@ -10,11 +10,17 @@ public class BookMapperImpl implements BookMapper {
 
     @Override
     public Book toBook(CreateBookRequest createBookRequest) {
+        if (createBookRequest == null) {
+            return null;
+        }
         return new Book(createBookRequest.getIsbn(), createBookRequest.getTitle());
     }
 
     @Override
     public BookDto toBookDto(Book book) {
+        if (book == null) {
+            return null;
+        }
         return new BookDto(book.getIsbn(), book.getTitle());
     }
 }
