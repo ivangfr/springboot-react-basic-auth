3feda6a439792cd7b16f023db586ea4eb2e7c310
Ivan Franchin <ivan.franchin@takeaway.com>
2024-07-09 15:11:43 +0200

    Project upgrade
    - upgrade to spring-boot 3.3.1;
    - upgrade wrapperVersion to 3.3.2 and apache-maven to 3.9.7;
    - upgrade to springdoc-openapi 2.6.0;
    - upgrade npm packages;
    - upgrade to postgres docker image 16.3;
    - remove explicityAuthenticationManager bean declaration;
    - update README.

--- book-api/src/main/java/com/ivanfranchin/bookapi/security/WebSecurityConfig.java
+++ book-api/src/main/java/com/ivanfranchin/bookapi/security/WebSecurityConfig.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,11 +17,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
